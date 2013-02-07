71d772158fe6d25f85313dabbf1329ed1f729ae4
Venkat Venkataraju <venkat.venkataraju@yahoo.com>
2013-02-06 18:20:38 -0800

    adding daemontools service

---
+++ arduino/serial_client/Feed.h
@@ -0,0 +1,12 @@
+#ifndef FEED_H
+#define FEED_H
+
+typedef struct {
+    unsigned int sw;
+    unsigned int red;
+    unsigned int green;
+    unsigned int blue;
+    unsigned int white;
+} Feed;
+
+#endif
